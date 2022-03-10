76339fc8767c6040b6772accbf41dbd71702bf26
Atya <nushnushka@gmail.com>
2022-03-10 19:38:55 -0300

    Initial commit

--- articles/admin.py
+++ articles/admin.py
@@ -8,3 +8,12 @@ from articles.models import Article
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'text')
+
+
+# heroku run python manage.py migrate
+
+# python manage.py dumpdata articles > D:\Nethology\articles\blog\articles\fixtures\DUMP.json
+
+# python manage.py dumpdata articles > DB_data.json
+
+
--- blog/settings.py
+++ blog/settings.py
@@ -84,7 +84,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -127,3 +126,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
