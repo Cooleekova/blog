fc9c229955b2ef835f78f539b1cbeecf1f25b747
Atya <nushnushka@gmail.com>
2022-03-17 17:10:07 -0300

    admin.py changed

--- articles/admin.py
+++ articles/admin.py
@@ -7,18 +7,6 @@ from articles.models import Article
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'text')
+    list_display = ('id', 'title', 'text', 'date_created')
     search_fields = ('title', 'text')
 
-# heroku run python manage.py migrate
-
-# python manage.py dumpdata articles > D:\Nethology\articles\blog\articles\fixtures\DUMP.json
-
-# python manage.py dumpdata articles > DB_data.json
-
-# heroku run python manage.py loaddata D:\Nethology\articles\blog\articles\fixtures\DUMP.json
-
-
-# heroku run python manage.py loaddata D:\Nethology\articles\blog\articles\fixtures\DUMP.json
-# D:\Nethology\articles\blog\articles\fixtures\DUMP.json
-# heroku run python manage.py loaddata articles/fixtures/DUMP.json
